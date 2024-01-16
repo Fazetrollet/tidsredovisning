d6848e93696cbfa36a7436214af123017a3b0908
Fazetrollet <154237719+Fazetrollet@users.noreply.github.com>
2024-01-16 13:20:53 +0200

    hämta alla aktiviteter fungerar

--- src/Activities.php
+++ src/Activities.php
@@ -39,6 +39,22 @@ function activities(Route $route, array $postData): Response {
  * @return Response
  */
 function hamtaAllaAktiviteter(): Response {
+    //koppla mot databas
+    $db= connectDb();
+    //hämta alla aktiviteter
+    $result = $db->query("SELECT id, namn From aktiviteter");
+
+    //Skapa returvärden
+    $retur=[];
+    foreach ($result as $item) {
+        $post=new stdClass();
+        $post->id=$item['id'];
+        $post->namn=$item['namn'];
+        $retur[]=$post;
+
+    }
+    //skicka svar
+    return new Response($retur);
 }
 
 /**
